a137bdd64040cb232abd1aefbeda7ad00c826e56
Allwrightben <ben.allwright@learningpeople.co.uk>
2024-06-21 20:15:06 +0100

    Secret key test and deploy

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3dt1ez2pqy*2k+5@w@7hw)!bw^ecndtq@ujk)hi#adx3fc*r3o'
+SECRET_KEY = 'djangocommand1%2&3*4^5@6!7(8)9_0+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -92,11 +92,11 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
-print(type(os.environ.get("DATABASE_URL")))
+
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
-print(type(os.environ.get("DATABASE_URL")))
+
 
 
 # Password validation
